c94d1110cfc6e35fd6056e824bf59614dc58bda0
Romeoscript <romeobourne211@gmail.com>
2022-12-18 15:55:06 +0100

    first commit

---
+++ rateswidget/getrates134e.php
@@ -0,0 +1,5 @@
+jQuery("document").ready(function() {jQuery(".conFixed_25D").html("<span class=\"v_rate\">6.990%</span><span class=\"rates_space\">&nbsp;&#47;&nbsp;</span><span  class=\"v_apr\" >7.136% </span>");
+jQuery(".fixedFHA").html("<span class=\"v_rate\">7.125%</span><span class=\"rates_space\">&nbsp;&#47;&nbsp;</span><span  class=\"v_apr\" >8.392% </span>");
+jQuery(".fixedUSDA").html("<span class=\"v_rate\">7.125%</span><span class=\"rates_space\">&nbsp;&#47;&nbsp;</span><span  class=\"v_apr\" >7.838% </span>");
+jQuery(".fixedVA").html("<span class=\"v_rate\">7.125%</span><span class=\"rates_space\">&nbsp;&#47;&nbsp;</span><span  class=\"v_apr\" >7.545% </span>");
+console.log("CalcApp - LOADED")});
